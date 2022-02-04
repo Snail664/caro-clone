fbc25c56068841f4aa644b3e9380fa6c75562c91
Greg-Lim <limgreg33cc@gmail.com>
2022-02-04 23:03:19 +0800

    Create ExploreCarousell
    made exploreCarousell for GREG
    removed greg.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import Hello from './components/Hello.js';
-import Greg from './components/Greg';
 
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Greg/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
