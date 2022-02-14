94019fc86fa99791948096e372eb61700d309fe7
Brandon Yeow <brandonywl@gmail.com>
2022-02-14 10:59:39 +0100

    Added carousel for slashed price items.

--- src/ProductsList/ProductListComponents/ProductCard.js
+++ src/ProductsList/ProductListComponents/ProductCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-const ProductCard = ({profilePic, userName, dateModified, prodImg, prodTitle, prodPrice, prodCondition, prodLikes, freeShipping, carouProtect}) => {
+const ProductCard = ({item}) => {
+  const {profilePic, userName, dateModified, prodImg, prodTitle, prodPrice, prodCondition, prodLikes, freeShipping, carouProtect} = item;
   return <div className="prod-card-wrapper">
     <div className="prod-card">
       <div className="prod-card-content">
--- src/ProductsList/ProductsList.js
+++ src/ProductsList/ProductsList.js
@@ -2,29 +2,37 @@ import React from "react";
 import ProductCard from './ProductListComponents/ProductCard';
 import './ProductsList.css';
 import productObj from './ProductListComponents/products.json';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 const ProductList = () => {
+  const newProdObj = [];
+  const prodObjCopy = [...productObj]
+
+  while (prodObjCopy.length) {
+    newProdObj.push(prodObjCopy.splice(0,5));
+  }
+
   return <div className="product-section">
     <p className="slashed-prices section-header">Slashed Prices</p>
+    <Carousel infiniteLoop={true}>
+    {newProdObj.map((row)=> {
+      return (
+        <div className="slashed-price-carousel-row">
+          {row.map((item)=>{
+            return <ProductCard item={item} />
+          })}
+        </div>
+      )
+    })}
+    </Carousel>
     <div className="recommended-for-you">
       <p className="section-header">Recommended For you</p>
       <div className="prod-list">
-        {productObj.map((item)=>{
-          console.log(item)
-          return (<ProductCard 
-            profilePic = {item.profilePic}
-            userName = {item.userName}
-            dateModified = {item.dateModified}
-            prodImg = {item.prodImg}
-            prodTitle = {item.prodTitle}
-            prodPrice = {item.prodPrice}
-            prodCondition = {item.prodCondition}
-            prodLikes = {item.prodLikes}
-            freeShipping = {item.freeShipping}
-            carouProtect = {item.carouProtect}
-            />)
-        })
-        }
+        {productObj.map((item)=>{ return (<ProductCard item={item} />)})}
+      </div>
+      <div className="view-more">
+        <button class="view-more-button">View More</button>
       </div>
     </div>
   </div>;
