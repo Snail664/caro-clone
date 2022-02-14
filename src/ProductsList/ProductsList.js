17d6e10be07c19413cdb84bf73b63fc5c926677c
Brandon Yeow <brandonywl@gmail.com>
2022-02-14 10:16:03 +0100

    Created Product Section with data.

--- src/ProductsList/ProductsList.js
+++ src/ProductsList/ProductsList.js
@@ -1,5 +1,33 @@
+import React from "react";
+import ProductCard from './ProductListComponents/ProductCard';
+import './ProductsList.css';
+import productObj from './ProductListComponents/products.json';
+
 const ProductList = () => {
-  return <div>Product List</div>;
+  return <div className="product-section">
+    <p className="slashed-prices section-header">Slashed Prices</p>
+    <div className="recommended-for-you">
+      <p className="section-header">Recommended For you</p>
+      <div className="prod-list">
+        {productObj.map((item)=>{
+          console.log(item)
+          return (<ProductCard 
+            profilePic = {item.profilePic}
+            userName = {item.userName}
+            dateModified = {item.dateModified}
+            prodImg = {item.prodImg}
+            prodTitle = {item.prodTitle}
+            prodPrice = {item.prodPrice}
+            prodCondition = {item.prodCondition}
+            prodLikes = {item.prodLikes}
+            freeShipping = {item.freeShipping}
+            carouProtect = {item.carouProtect}
+            />)
+        })
+        }
+      </div>
+    </div>
+  </div>;
 };
 
 export default ProductList;
