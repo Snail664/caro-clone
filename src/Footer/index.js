a449ef7c5cc5a9bc82d7d3e4a863c22834be6a7d
Samuel <schua045@e.ntu.edu.sg>
2022-02-17 21:07:46 +0800

    added footer

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,5 +1,71 @@
+import "./footer.css";
+
 const Footer = () => {
-  return <div>footer</div>;
+  return <div class="root">
+    <div class="D_cw">
+      <div class="D_iz D_iv">
+        <div class="D_jS">
+          <div class="D_jT">
+            <div><div class="D_wrapper"><div class="D_uC">
+              <div class="D_oN" style={{ backgroundImage: `url("https://storage.googleapis.com/carousell-sl/homescreens/main/carousell_qrcode_background.png")` }}>
+                <div class="D_oP"><img alt="" class="D_fC D_f_ D_oQ" src="https://storage.googleapis.com/carousell-sl/homescreens/main/carousell_qrcode_cats.png" title="" /></div><div class="D_oR"><h1 class="D_fq D_ey D_fv D_fy D_fn">Everyone Wins on Carousell</h1><p class="D_hz D_ez D_h_ D_hC D_hG D_hI D_hK D_oT D_hp">Unlock exclusive features when you download the Carousell app today!</p><div class="D_oS"><div class="D_oU">
+
+                  <a class="D_hX" href="https://itunes.apple.com/app/id548607187" rel="noopener noreferrer" target="_blank">
+                    <img alt="badge_ios" class="D_fC D_f_ D_oW" src="https://storage.googleapis.com/carousell-sl/homescreens/main/carousell_qrcode_apple_store.svg" title="" /></a>
+                  <a class="D_hX" href="https://play.google.com/store/apps/details?id=com.thecarousell.Carousell" rel="noopener noreferrer" target="_blank">
+                    <img alt="badge_android" class="D_fC D_f_ D_oW" src="https://storage.googleapis.com/carousell-sl/homescreens/main/carousell_qrcode_gplay.svg" title="" /></a></div>
+                  <img alt="" class="D_fC D_f_ D_oV" src="https://storage.googleapis.com/carousell-sl/homescreens/main/carousell_qrcode_branch.png" title="" />
+                </div></div></div></div></div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="D_pm">
+      <div class="D_qy">
+        <div class="D_qz">
+          <svg class="D_qA" height="24" viewBox="0 0 80 80" width="24" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path d="M69.57 6.55V3.69A3.69 3.69 0 0065.87 0h-9.3a3.69 3.69 0 00-3.7 3.7v2.04H15.05C10.21 5.74 3 8.94 3 17.79v49.83c0 4.85 3.21 12.06 12.05 12.06H64.9c4.85 0 12.05-3.21 12.05-12.06V17.8c0-3.87-2.05-9.23-7.37-11.24" fill="#ff2636"></path><path d="M63.64 40.5h-6.1a2.2 2.2 0 100 4.42h6.1a2.21 2.21 0 100-4.42m-9.63-4.04a2.2 2.2 0 002.91 1.12l5.57-2.48a2.2 2.2 0 10-1.8-4.04l-5.56 2.48A2.2 2.2 0 0054 36.46m-.64-5.33l4.08-4.53a2.2 2.2 0 10-3.28-2.96l-4.08 4.53a2.2 2.2 0 00.16 3.12 2.2 2.2 0 003.12-.16" fill="#ffffff"></path><path d="M47.5 58.74a2.2 2.2 0 00-2.78-1.42 15.37 15.37 0 11-4.75-29.97 15.37 15.37 0 014.75.75 2.21 2.21 0 002.78-1.42l1.89-5.8a2.21 2.21 0 00-1.42-2.78 25.87 25.87 0 00-8-1.27 25.88 25.88 0 108 50.5 2.2 2.2 0 001.42-2.8l-1.89-5.8" fill="#ffffff"></path><path d="M53.37 54.29a2.21 2.21 0 00-3.28 2.96l4.08 4.53a2.21 2.21 0 003.28-2.96l-4.08-4.53m9.13-3.97l-5.58-2.48a2.21 2.21 0 00-1.8 4.03l5.58 2.49a2.21 2.21 0 001.8-4.04" fill="#ffffff"></path></g></svg>
+          <p class="D_hz D_ev D_h_ D_hC D_hG D_hI D_hK D_qC D_hs">© 2022 Carousell</p>
+          <div class="D_qE"><a class="D_hX" href="https://support.carousell.com/hc/en-us?origin=web&amp;platform=web" rel="noopener noreferrer" target="_blank"><p class="D_hz D_ev D_h_ D_hC D_hF D_hI D_hK D_jG D_jK D_hs">Help Centre</p></a><p class="D_hz D_ex D_h_ D_hC D_hG D_hI D_hK D_hs">&nbsp;
+            •
+            &nbsp;</p><a class="D_hX" href="https://support.carousell.com/hc/en-us/requests/new?open_chat_bot=true&amp;origin=web&amp;platform=web" rel="noopener noreferrer" target="_blank"><p class="D_hz D_ev D_h_ D_hC D_hF D_hI D_hK D_jG D_jK D_hs">Contact Us</p></a><p class="D_hz D_ex D_h_ D_hC D_hG D_hI D_hK D_hs">&nbsp;
+              •
+              &nbsp;</p><a class="D_hX" href="https://press.carousell.com" rel="noopener noreferrer" target="_blank"><p class="D_hz D_ev D_h_ D_hC D_hF D_hI D_hK D_jG D_jK D_hs">Press</p></a><p class="D_hz D_ex D_h_ D_hC D_hG D_hI D_hK D_hs">&nbsp;
+                •
+                &nbsp;</p><a class="D_hX" href="https://careers.carousell.com/" rel="noopener noreferrer" target="_blank"><p class="D_hz D_ev D_h_ D_hC D_hF D_hI D_hK D_jG D_jK D_hs">Jobs</p></a><p class="D_hz D_ex D_h_ D_hC D_hG D_hI D_hK D_hs">&nbsp;
+                  •
+                  &nbsp;</p><a class="D_hX" href="https://docs.google.com/forms/d/1ZhprlP1FZ7Jk55RqlkAnT6YcV9DTDUoFM1gLCM59L6A/" rel="noopener noreferrer" target="_blank"><p class="D_hz D_ev D_h_ D_hC D_hF D_hI D_hK D_jG D_jK D_hs">Advertise with Us</p></a><p class="D_hz D_ex D_h_ D_hC D_hG D_hI D_hK D_hs">&nbsp;
+                    •
+                    &nbsp;</p><a class="D_hX" href="https://support.carousell.com/hc/en-us/articles/115011881808" rel="noopener noreferrer" target="_blank"><p class="D_hz D_ev D_h_ D_hC D_hF D_hI D_hK D_jG D_jK D_hs">Terms</p></a><p class="D_hz D_ex D_h_ D_hC D_hG D_hI D_hK D_hs">&nbsp;
+                      •
+                      &nbsp;</p><a class="D_hX" href="https://support.carousell.com/hc/en-us/articles/115006700307" rel="noopener noreferrer" target="_blank"><p class="D_hz D_ev D_h_ D_hC D_hF D_hI D_hK D_jG D_jK D_hs">Privacy</p></a></div>
+        </div>
+
+        <div class="D_q_">
+        <div class="D_qF">
+          <a class="D_hX" href="https://au.carousell.com" rel="noopener noreferrer" target="_blank"><span class="D_f_ D_qG D_fA"><img alt="Australia flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-au-1Vu2Ly0cBR.jpg" title="" />
+          </span></a><a class="D_hX" href="https://ca.carousell.com" rel="noopener noreferrer" target="_blank"><span class="D_f_ D_qG D_fA"><img alt="Canada flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-ca-3zQT2z3CIo.jpg" title="" />
+          </span></a><a class="D_hX" href="https://www.carousell.com.hk" rel="noopener noreferrer" target="_blank">
+            <span class="D_f_ D_qG D_fA"><img alt="Hong Kong flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-hk-1Fq4xKBpW1.jpg" title="" />
+            </span></a><a class="D_hX" href="https://id.carousell.com" rel="noopener noreferrer" target="_blank"><span class="D_f_ D_qG D_fA">
+              <img alt="Indonesia flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-id-3hB56YEmaV.jpg" title="" />
+            </span></a><a class="D_hX" href="https://www.carousell.com.my" rel="noopener noreferrer" target="_blank">
+            <span class="D_f_ D_qG D_fA"><img alt="Malaysia flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-my-GKdsVkW7Qq.jpg" title="" />
+            </span></a><a class="D_hX" href="https://nz.carousell.com" rel="noopener noreferrer" target="_blank"><span class="D_f_ D_qG D_fA">
+              <img alt="New Zealand flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-nz-24qlj462lw.jpg" title="" /></span></a>
+          <a class="D_hX" href="https://www.carousell.ph" rel="noopener noreferrer" target="_blank"><span class="D_f_ D_qG D_fA">
+            <img alt="Philippines flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-ph-cYUSK6Hapb.jpg" title="" />
+          </span></a><a class="D_hX" href="https://www.carousell.sg" rel="noopener noreferrer" target="_blank"><span class="D_f_ D_qG D_fA">
+            <img alt="Singapore flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-sg-3PPSOjFS5a.jpg" title="" /></span></a>
+          <a class="D_hX" href="https://tw.carousell.com" rel="noopener noreferrer" target="_blank"><span class="D_f_ D_qG D_fA">
+            <img alt="Taiwan flag icon" class="D_fC" src="https://mweb-cdn.karousell.com/build/flag-tw-6cZEDUfY6q.jpg" title="" /></span></a></div><div class="D_qI"><select class="D_AK D_AI D_AH"><option value="en">English</option><option value="zh-Hant-TW">繁體中文 (台灣)</option><option value="zh-Hant-HK">繁體中文 (香港)</option><option value="id">Bahasa Indonesia</option></select></div>
+            </div>
+
+      </div>
+
+
+    </div>
+  </div>;
 };
 
 export default Footer;
