54e52d044e7924e8d4e4fb341013640286da171e
EyeCantSee <66559292+EyeCantSee@users.noreply.github.com>
2022-02-20 21:40:30 +0800

    Added navbar
    Install react-router-dom, react-icons and styed-components before running

--- README.md
+++ README.md
@@ -68,3 +68,7 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+### Prerequisites
+
+Install react-router-dom, react-icons and styled-components before running
--- src/App.js
+++ src/App.js
@@ -2,19 +2,31 @@ import "./App.css";
 import BlackBar from "./BlackBar/BlackBar";
 import WhiteBar from "./WhiteBar/WhiteBar";
 import CategorySlider from "./CategorySlider/CategorySlider";
+import React from 'react';
 import ProductList from "./ProductsList/ProductsList";
 import Gallery from "./Gallery";
 import Footer from "./Footer";
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages'
+import About from './pages/about'
 
 function App() {
   return (
-    <div className="App">
-      <BlackBar />
-      <WhiteBar />
-      <Gallery />
-      <CategorySlider />
-      <ProductList />
-      <Footer />
+    <div className="App" > 
+    <Router>
+    <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About/>} />
+      </Routes>
+    </Router>
+    {/* <BlackBar /> */}
+    <WhiteBar />
+    <Gallery />
+    <CategorySlider />
+    <ProductList />
+    <Footer />
     </div>
   );
 }
